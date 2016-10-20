89e5d1e20a535cb0fc484fd40dc3a21ca18273e5
Sreyas <sreyassreelal@gmail.com>
2016-10-19 18:00:59 -0700

    Update register.cpp

--- Source/register.cpp
+++ Source/register.cpp
@@ -1,12 +1,11 @@
-
-
 /*
 Register system for console games by Sreyas
 
 */
-//global vars
-int data_size,_logined=0,_connected=0,P_INDEX;//saving data size in a var is much more optimised than use tellg()
-//global functions
+// global vars
+int data_size, _logined = 0, _connected = 0,
+               P_INDEX; // saving data size in a var is much more optimised than use tellg()
+// global functions
 void Register();
 
 void Login();
@@ -15,34 +14,31 @@ void Save();
 
 void connect();
 
-//class for storing player data
+// class for storing player data
 
 class P_DATA
 {
- public:
- char name[24],password[24];
- int pscore;
+public:
+    char name[24], password[24];
+    int pscore;
 
- P_DATA()
- {
-  pscore = 0;
- }
+    P_DATA()
+    {
+        pscore = 0;
+    }
 
- void P_GET()
- {
-  cout<<"Input your name ";
-  gets(name);
-  cout<<"Input password ";
-  gets(password);
- }
+    void P_GET()
+    {
+        cout << "Input your name ";
+        gets(name);
+        cout << "Input password ";
+        gets(password);
+    }
 
- ~P_DATA()
- {
-
- }
-
+    ~P_DATA()
+    {
+    }
 };
-
 
 
 ////////////////////////////////////////////////////////////////////////////
